cbf0119fd81ed20055833b89e94a14e22851f3ab
wdk59 <dookong59@gmail.com>
2022-11-04 18:25:20 +0900

    IOS->Android
    ㅈㄱㄴ

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -7,7 +7,7 @@ public class CubeScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("SH");
+        Debug.Log("JSH");
     }
 
     // Update is called once per frame
