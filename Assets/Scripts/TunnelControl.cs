e9a56c89162bc5bf4998764749379e88b9af65dd
Yoonsin <zpssol3@naver.com>
2022-12-18 15:59:27 +0900

    Revert "스위치 이동 구현"
    This reverts commit 9595dd04723a693e0f3aaab4f08fbd63da9a00ae.

--- Assets/Scripts/TunnelControl.cs
+++ Assets/Scripts/TunnelControl.cs
@@ -15,7 +15,7 @@ public class TunnelControl : MonoBehaviour
     {
         Tun = false;
         light = GetComponentInChildren<Light2D>();
-        Invoke("TurnOn", 5);
+        Invoke("TurnOn", 50000);
     }
 
     
