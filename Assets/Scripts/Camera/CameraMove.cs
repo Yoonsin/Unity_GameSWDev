c45c846d1e6cf50259368f2a4fdce45b7819d90d
Gyuseob Song <gs2412@naver.com>
2023-01-09 17:37:27 +0900

    코드, 오브젝트 재배치
    카메라 부모 오브젝트가 플레이어 위치 추적

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -9,6 +9,15 @@ public class CameraMove : MonoBehaviour
     Vector3 pos;
     public GameObject player;
 
+
+    public float cameraSetX = 0;
+    public float cameraSetY = 3;
+    public float cameraSetZ = -10; //대상 오브젝트로부터 얼마나 x,y,z 좌표만큼 카메라가 떨어져 있을 건지 초기 설정
+
+    public float cameraSpeed = 6; //카메라 움직이는 속도
+
+    Vector3 cameraPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +25,18 @@ public class CameraMove : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        cameraPosition.x = player.transform.position.x + cameraSetX;
+        cameraPosition.y = player.transform.position.y + cameraSetY;
+        cameraPosition.z = player.transform.position.z + cameraSetZ;
+
+        transform.position = Vector3.Lerp(transform.position, cameraPosition, cameraSpeed * Time.smoothDeltaTime);
     }
 
     void LateUpdate()
     {
+        /*
         if(player.GetComponent<PlayerMove>().isShaked==false) //카메라가 흔들릴 때는 카메라가 플레이어 따라가는거 잠시 멈추기 
         {
             pos = player.transform.position; //플레이어 위치 받아오기
@@ -31,7 +45,7 @@ public class CameraMove : MonoBehaviour
             pos.z = -10;
             this.transform.position = pos; //카메라 위치에 플레이어 위치 넣어서 플레이어에 따라 카메라 이동시켜주기
         }
-        
-        
+        */
+
     }
 }
